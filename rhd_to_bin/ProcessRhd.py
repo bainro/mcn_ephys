fcfe9678c57d68a23c0ccf0296ed1efd53daf11c
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-01-20 22:22:37 -0800

    Update ProcessRhd.py

--- rhd_to_bin/ProcessRhd.py
+++ rhd_to_bin/ProcessRhd.py
@@ -41,13 +41,16 @@ def channel_shift(data, sample_shifts):
 if __name__ == "__main__":
     subsample_factors = [5, 6]
     subsample_total = numpy.prod(subsample_factors)
+    print()
     use_default = input(f"Use the default downsampling factor of {subsample_total}? (y/n) ")
     use_default= ('y' in use_default) or ('Y' in use_default)
     if not default_factor:
+        print()
         subsample_total = int(input("What downsampling factor would you like to use then? "))
         # see downsample() for reasoning
         if subsample_total > 13:
             power = math.ceil(subsample_total / 13)
+            print()
             help_txt = f"Downsampling by this much requires smaller steps. " 
             help_txt += f"List {power} factors separated by commas that multiply to {subsample_total}. "
             help_txt += 'E.g. 5, 6 for a downsampling factor of 30'
@@ -56,9 +59,11 @@ if __name__ == "__main__":
             tot = np.prod(subsample_factors)
             err_txt = f'{subsample_factors} multiply to {tot}, not {subsample_total}'
             assert tot == subsample_total, err_txt
-        
+
+    print()
     dirs_txt = "You can specify multiple directories to process. " 
-    dirs_txt += "It is assumed that each directory had .rhd files for one animal recording. "
+    dirs_txt += "It is assumed that each directory has .rhd files for one animal recording. "
+    dirs_txt += "It is also assumed that all recordings have the same probe setup. "
     dirs_txt += "List your directories separated by commas. E.g. C:\animal1_day1, C:\animal2_day6"
     dirs = input(dir_txt).replace(", ", ",").split(',')
     # in case someone is silly enough to append trailing comma
@@ -68,16 +73,19 @@ if __name__ == "__main__":
     save_dir = input('Where would you like to save the outputs?')
     os.makedirs(save_dir, exist_ok=True)
     save_dir = os.path.abspath(save_dir)
-    
+
+    print()
     saveLFP = input('Would you like to save the LFP? (y/n) ')
     saveLFP = ('y' in saveLFP) or ('Y' in saveLFP)
+    print()
     saveAnalog = input('Would you like to save the analog signal? (y/n) ')
     saveAnalog = ('y' in saveAnalog) or ('Y' in saveAnalog)
 
     # ask for this user input before the long loop below
+    print()
     animals = []
     for d in dirs:
-        name = input(f"What's the animal's ID in {d}?")
+        name = input(f"What is the animal's ID for {d}?")
         animals.append(name)
 
     overwrite = None
@@ -89,11 +97,15 @@ if __name__ == "__main__":
         lfpts_filename = os.path.join(sub_save_dir, animal_id+'-lfpts.npy')
         digIn_filename = os.path.join(sub_save_dir, animal_id+'-digIn.npy')
         analogIn_filename = os.path.join(sub_save_dir, animal_id+'-analogIn.npy')
-        # Allows resuming after a crash or error
-        ### @TODO check different file, because they might not have saved this one!
-        old_data = os.path.isfile(analogIn_filename)
+        # can protect data or allow quickly resuming after an error
+        old_data = glob.glob(os.path.join(sub_save_dir,'*shifted.bin'))
         if old_data:
-            
+            if overwrite == None:
+                print()
+                overwrite = input('Previous output found. Overwrite old binaries? (y/n) ')
+                overwrite = ('y' in overwrite) or ('Y' in overwrite)
+            if overwrite == False:
+                continue               
         
         files = natsorted(glob.glob(os.path.join(dirname,rawfname,'*.rhd')))
         amp_data = read_data(os.path.join(dirname,rawfname,files[0]))[1]
@@ -154,4 +166,4 @@ if __name__ == "__main__":
             np.save(digIn_filename, dig_in)
 
 ### @TODO combine all .bin ???
-    # might compare speed of DxH (abc?) to python solution
+    # might 1st compare speed of DxH (abc?) to python solution
