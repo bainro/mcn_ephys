40d51bf48e89fdfebb86b5a38276f60ad1a62ae2
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-01-23 01:51:56 -0800

    Delete rhd_to_bin/slow_bin_merge.py

--- rhd_to_bin/slow_bin_merge.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-path1 = os.path.join(sys.argv[1])
-path2 = os.path.join(sys.argv[2])
-
-with open(path1, "ab") as file1, open(path2, "rb") as file2:
-    file1.write(file2.read())
