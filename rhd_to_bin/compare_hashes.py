2eff111e3fefebb3f8f0cdbc7b59960688791995
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-02-08 17:04:25 -0800

    Update compare_hashes.py

--- rhd_to_bin/compare_hashes.py
+++ rhd_to_bin/compare_hashes.py
@@ -25,3 +25,4 @@ if __name__ == "__main__":
     for f in sys.argv[1:]:
         files.append(os.path.join(f))
     assert hash_files(files), "Files are different :("
+    print("Files are identical :)")
