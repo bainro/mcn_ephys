3be890776b7550590587324a3cd79a14df27185f
Robert Bain <31903812+bainro@users.noreply.github.com>
2024-02-08 17:03:21 -0800

    Update compare_hashes.py

--- rhd_to_bin/compare_hashes.py
+++ rhd_to_bin/compare_hashes.py
@@ -2,24 +2,26 @@ import os
 import sys
 import hashlib
 
-files = []
-for f in sys.argv[1:]:
-    files.append(os.path.join(f))
+def hash_files(files):
+    digests = []
+    for filename in files:
+        hasher = hashlib.md5()
+        with open(filename, 'rb') as f:
+            buf = f.read()
+            hasher.update(buf)
+            a = hasher.hexdigest()
+            digests.append(a)
+            print(a)
+    
+    for i, d1 in enumerate(digests[:-1]):
+        d2 = digests[i+1]
+        f1 = files[i]
+        f2 = files[i+1]
+        return False
+    return True
 
-digests = []
-for filename in files:
-    hasher = hashlib.md5()
-    with open(filename, 'rb') as f:
-        buf = f.read()
-        hasher.update(buf)
-        a = hasher.hexdigest()
-        digests.append(a)
-        print(a)
-
-for i, d1 in enumerate(digests[:-1]):
-    d2 = digests[i+1]
-    f1 = files[i]
-    f2 = files[i+1]
-    assert d1 == d2, f"hashes for {f1} & {f2} differ :("
-
-print("congrats! all files are identical :)")
+if __name__ == "__main__":
+    files = []
+    for f in sys.argv[1:]:
+        files.append(os.path.join(f))
+    assert hash_files(files), "Files are different :("
